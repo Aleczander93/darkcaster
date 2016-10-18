8fcaa5eb0e3d0abf8d05247c5789fa9355fcefc5
aleczander <afigueroa81@ufl.edu>
2016-10-17 20:55:54 -0400

    did so much im not entirely sure what happened i have to recap tomorrow

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 var express = require ('express');
 var server = express();
+var logger = require('./middleware/logger.js');
+var cors = require('cors');
+var apiKey = require ('./config.js').apiKey;
+
 var port = process.env.PORT || 8080;
 
 server.use(express.static(__dirname + '/public'));
+server.use(logger);
+server.use(cors());
+var axios = require('axios');
 
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root:__dirname});
+});
+
+server.get('/forecast/:latitude,:longitude', function(request, response){
+  var url = 'https://api.darksky.net/forecast/'+ apiKey +'/'+ request.params.latitude +',-122.4233';
+  var timeoutConfig = {
+    timeout: 2000
+  };
+  axios.get(url, timeoutConfig)
+    .then(function(forecast){ //when it's successful
+      response.send(forecast.data);
+    })
+    .catch(function(error){ //when not successful
+      response.send(error);
+    });
 });
 
 server.listen(port, function(){
